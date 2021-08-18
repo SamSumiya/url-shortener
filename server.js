d3f7b102b6a7e1492e6ffa64c974fc1a1b33d7cc
chenerychen <60025491+chenerychen@users.noreply.github.com>
2021-08-18 09:57:03 -0400

    it worked!!

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
+require('dotenv').config()
 const express = require('express')
-const http = require('http') 
+const http = require('http')
+const mongoose = require('mongoose')
+const shortUrl = require('./models/ShortURL.js')
 const app = express()
 const server = http.createServer(app)
 
+mongoose.connect('mongodb://localhost/urlShortner', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.set("view engine", 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-   res.render('index')
+app.get('/', async(req, res) => {
+  const shortUrls = await shortUrl.find()
+  res.render('index', { shortUrls })
 })
 
+app.post('/shortUrls', async (req, res) => {
+    await shortUrl.create({ full: req.body.fullUrl })
+    res.redirect('/')    
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const response = await shortUrl.findOne({ short: req.params.shortUrl });
+  if (response === null) return res.sendStatus(404)
+
+  response.clicks++;
+  response.save();
+  res.redirect(response.full)
+})
 
 const PORT = process.env.PORT || 5050
 
